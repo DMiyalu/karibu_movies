d4d44a9e3353b03ea52201d16431fc5ce5bfe2a5
DMiyalu <dieudonnemiyalu6@gmail.com>
2021-03-10 13:06:58 +0100

    theme data

---
+++ karibu_movies/lib/screens/home.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  Home({Key key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text(
+        'Home Screen',
+        style: Theme.of(context)
+            .textTheme
+            .headline6
+            .copyWith(color: Theme.of(context).accentColor),
+      ),
+    );
+  }
+}
